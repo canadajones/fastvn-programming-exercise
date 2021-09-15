d5d49a60abaebc9771c46caf0eb1eb83af323c76
canadajones <canadajones68@gmail.com>
2021-09-15 23:54:16 +0200

    Actually managed to include the right Boost version this time!

--- loader.h
+++ loader.h
@@ -1,12 +1,18 @@
 #include <vector>
 #include <string>
+#include <stdexcept>
+
+#include <boost/json.hpp>
+
+#include "chapter.h"
 
 #ifndef GENERIC_LOADER_HEADER
 #define GENERIC_LOADER_HEADER
 
 
 
-
+namespace vnpge {
+	namespace json = boost::json;
 class ChapterLoader {
 	private:
 	std::vector<std::string> paths;
@@ -18,9 +24,17 @@ class ChapterLoader {
 class JSONLoader {
 	private:
 	std::vector<std::string> paths;
-
+	
 	public:
+	JSONLoader(std::vector<std::string>& paths) : paths{paths} {
+		if (paths.size() < 1) {
+			throw std::runtime_error("No paths to JSON files were specified!");
+		}
+	}
 
+	// Get the latest version of Boost installed!
+	// Then, find something to load files into a string
+	// Lastly, parse the strings and construct them into a Chapter
 };
-
+}
 #endif
