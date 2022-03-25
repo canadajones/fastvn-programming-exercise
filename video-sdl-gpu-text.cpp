b9e956bd072be28238fc0412e032edf55dbe1eaa
canadajones <48071533+canadajones@users.noreply.github.com>
2022-03-26 00:41:26 +0100

    Began work on the GPU accelerated renderer

---
+++ video-sdl-gpu-text.cpp
@@ -0,0 +1,243 @@
+module;
+#include <SDL2/SDL_render.h>
+#include <limits>
+#include <memory>
+#include <stdexcept>
+#include <functional>
+
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_surface.h>
+#include <SDL2/SDL_rect.h>
+#include <SDL2/SDL_blendmode.h>
+#include <SDL2/SDL_pixels.h>
+
+#include <SDL2/SDL_image.h>
+
+#include <SDL2/SDL_ttf.h>
+
+#include "video-sdl-common.h"
+
+#include "structures.h"
+
+
+export module AcceleratedText;
+
+import StoryDialogue;
+
+
+export namespace vnpge {
+
+	class GPUFont : DialogueFont {
+	private:
+	std::shared_ptr<TTF_Font> font;
+
+	public:
+	GPUFont(DialogueFont& dfont, uint ptSize) : DialogueFont(dfont) {
+		float hdpi, vdpi;
+		SDL_GetDisplayDPI(0, nullptr, &hdpi, &vdpi);
+		font = {TTF_OpenFontDPI(dfont.getName().c_str(), ptSize, hdpi, vdpi), TTF_CloseFont };
+	};
+
+	GPUFont() = default;
+	TTF_Font* getFont() {
+		return font.get();
+	}
+
+	~GPUFont() {};
+	};
+
+class TextBoxInfo {
+	private:
+	AbsoluteDimensions resolution;
+	RelativeDimensions relativeArea;
+
+	public:
+	TextBoxInfo(AbsoluteDimensions resolution, RelativeDimensions area) : resolution{resolution}, relativeArea{area} {};
+
+	AbsoluteDimensions getResolution() {
+		return resolution;
+	};
+
+	RelativeDimensions getArea() {
+		return relativeArea;
+	};
+};
+
+
+class FontStorage {
+	private:
+	std::unordered_map<std::string, vnpge::GPUFont> fontMap;
+	static uint getPtSize(const AbsoluteDimensions& boxDims) {
+		double boxHeight = static_cast<double>(boxDims.h) * 0.25;
+		uint ptSize;
+		uint numLines = 5;
+
+		// TODO: Evaluate pixel values based on DPI
+
+		while (boxHeight > 40) {
+			ptSize = boxHeight / numLines;
+			if (ptSize > 60) {
+				numLines += 1;
+				continue;
+			}
+			if (ptSize < 15) {
+				numLines -= 1;
+				continue;
+			}
+
+			if (boxHeight / (numLines + 1) < 35) {
+				break;
+			}
+			else {
+				numLines += 1;
+			}
+		}
+		numLines += static_cast<uint>(boxDims.h / boxDims.w);
+		return static_cast<uint>(std::round(static_cast<double>(boxHeight) / static_cast<double>(numLines))) * 4;
+	}
+	public:
+	GPUFont operator() (DialogueFont font, const AbsoluteDimensions& boxDims ) {
+		if (!fontMap.contains(font.getName())) {
+			fontMap.insert({font.getName(), {font, getPtSize(boxDims)}});
+		}
+		return fontMap.at(font.getName());
+	}
+
+	void clear() {
+		fontMap.clear();
+	}
+};
+
+
+
+
+
+
+/**
+ * @brief Stateful renderer for textboxes
+ * 
+ */
+class TextRenderer {
+	private:
+
+	// The destination surface
+	SDL_Surface* dest;
+	
+
+	std::shared_ptr<SDL_Surface> background;
+	std::shared_ptr<SDL_Surface> text;
+
+	int scrolledLines = 0;
+	int lineHeight;
+
+	FontStorage fontStorage;
+
+	// Generated values, don't touch
+
+	AbsoluteDimensions textArea;
+	AbsolutePosition textPosition; // origin is at upper left of background
+	
+	public:
+	TextRenderer(SDL_Surface* dest, TextBoxInfo boxInfo, TextBGCreator<SDL_Surface*> bgCreator,
+				 Dialogue dialogue, DialogueFont font) {
+		updateResolution(dest, boxInfo, bgCreator);
+		renderStoryFrame(dialogue, font);
+	};
+	
+	SDL_Surface* renderStoryFrame(Dialogue dialogue, DialogueFont font) {
+
+		// Grab dialogue colour
+		SDL_Color fgcolour = {
+			.r = static_cast<uint8_t>(dialogue.getColour().red),
+			.g = static_cast<uint8_t>(dialogue.getColour().green),
+			.b = static_cast<uint8_t>(dialogue.getColour().blue),
+			.a = 255
+		};
+
+		// Look up font in font table
+		GPUFont f = fontStorage(font, textArea);
+
+		// Temporary storage for the complete text
+		SDL_Surface* renderedText = TTF_RenderUTF8_Blended_Wrapped(f.getFont(), dialogue.getText().c_str(), fgcolour, textArea.w);
+		
+		// Store the updated text
+		text.reset(renderedText, SDL_FreeSurface);
+
+		// Grab the line height of the text, for scrolling purposes
+		lineHeight = TTF_FontLineSkip(f.getFont());
+
+		resetScroll();
+
+		return text.get();
+	};
+
+	void updateResolution(SDL_Surface* newDest, TextBoxInfo boxInfo, TextBGCreator<SDL_Surface*> bgCreator) {
+		dest = newDest;
+
+		// Generate a new text background, complete with information about position and area available to actual text
+		auto textBGSurface = bgCreator(boxInfo.getResolution(), boxInfo.getArea());
+		
+		background.reset(textBGSurface.first, SDL_FreeSurface);
+
+		textArea = textBGSurface.second.area;
+		textPosition = textBGSurface.second.position;
+
+		fontStorage.clear();
+	};
+
+	void displayText(AbsolutePosition position) {
+		
+		SDL_Rect destPos = {
+			.x = position.x,
+			.y = position.y,
+		};
+		
+		// Put box on screen (easy!)
+		SDL_BlitSurface(background.get(), nullptr, dest, &destPos);
+
+		// Offset box pos by text position within the box
+		destPos = {
+			.x = position.x + textPosition.x,
+			.y = position.y + textPosition.y
+		};
+
+		// Pick a rectangle representing the current scrolled level
+		// x is always 0
+		// y is text scroll
+		// w and h are given by the box generator function as the writable part of the box
+		SDL_Rect srcPos = {
+			.x = 0,
+			.y = scrolledLines * lineHeight,
+			.w = static_cast<int>(textArea.w),
+			.h = static_cast<int>(textArea.h)
+		};
+		
+		// Put text on screen (less easy!)
+		SDL_BlitSurface(text.get(), &srcPos, dest, &destPos);
+	};
+
+	/**
+	 * @brief Scroll text up by a line. If scrolling would reveal no new text, do nothing.
+	 * 
+	 */
+	void scrollTextUp() {
+		if (static_cast<int>(textArea.h) < text->h && (scrolledLines + 1) * lineHeight < text->h) {
+			scrolledLines++;
+		}
+	};
+	/**
+	 * @brief Scroll text down by a line. If scrolling would reveal no new text, do nothing.
+	 * 
+	 */
+	void scrollTextDown() {
+		if (scrolledLines > 0) {
+			scrolledLines--;
+		}
+	}
+
+	void resetScroll() {
+		scrolledLines = 0;
+	}
+};
+
+};
---
+++ video-sdl-gpu.cpp
@@ -0,0 +1,299 @@
+module;
+
+#include <memory>
+#include <vector>
+#include <string>
+#include <exception>
+#include <unordered_map>
+
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_video.h>
+#include <SDL2/SDL_surface.h>
+#include <SDL2/SDL_rect.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_ttf.h>
+
+#include "video-sdl-common.h"
+
+
+#include "structures.h"
+#include "schedule.h"
+
+
+export module AcceleratedRender;
+
+import Chapter;
+import Image;
+import StoryDialogue;
+import AcceleratedText;
+
+
+export namespace vnpge {
+
+template<typename T>
+void nullDeleter(T* surf) {
+};
+
+class GPUImage : Image {
+private:
+	std::shared_ptr<SDL_Texture> texture;
+public:
+	GPUImage(Image& baseImage, SDL_Renderer* renderer) : Image{ baseImage } {
+		SDL_Surface* surf = IMG_Load(baseImage.path.c_str());
+
+		texture = {SDL_CreateTextureFromSurface(renderer, surf), SDL_DestroyTexture};
+
+		SDL_FreeSurface(surf);
+
+		std::cout << "new texture loaded" << std::endl; 
+	};
+
+	SDL_Texture* getTexture() {
+		return texture.get();
+	}
+};
+
+class GPURenderManager {
+private:
+	SDL_Window* window;
+	SDL_Renderer* renderer;
+
+	std::unordered_map<std::string, GPUImage> imageMap;
+public:
+	GPURenderManager() {
+		// Initialize SDL
+		if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO) < 0) {
+			std::string err = "SDL could not initialize! SDL_Error: ";
+			throw std::runtime_error(err.append(SDL_GetError()));
+		}
+
+		// Load support for the JPG and PNG image formats
+		int flags = IMG_INIT_JPG | IMG_INIT_PNG;
+		if (IMG_Init(flags) ^ flags) {
+			std::string err = "SDL_Image could not initialize! IMG_Error: ";
+			throw std::runtime_error(err.append(IMG_GetError()));
+		}
+
+		// Grab font rendering library
+		if (TTF_Init()) {
+			std::string err = "TTF_Init: ";
+			throw std::runtime_error(err.append(TTF_GetError()));
+		}
+
+		//Create window
+		//window = SDL_CreateWindow("test", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 1000, 800, SDL_WINDOW_FULLSCREEN_DESKTOP);
+
+		window = SDL_CreateWindow("test", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 1000, 600, SDL_WINDOW_RESIZABLE);
+
+		// If window creation failed, crash and burn
+		if (window == nullptr) {
+			std::string err = "Window could not be created! SDL_Error: ";
+			throw std::runtime_error(err.append(SDL_GetError()));
+		}
+
+		renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+
+		if (renderer == nullptr) {
+			std::string err = "Renderer could not be created! SDL_Error: ";
+			throw std::runtime_error(err.append(SDL_GetError()));
+		}
+	};
+
+	~GPURenderManager() {
+		// Unload font support
+		TTF_Quit();
+
+		// Unload image format loading support
+		IMG_Quit();
+
+		// Destroy window
+		SDL_DestroyWindow(window);
+
+		// Quit SDL subsystems
+		SDL_Quit();
+	};
+
+
+	SDL_Window* getWindow() {
+		return window;
+	};
+
+	SDL_Renderer* getRenderer() {
+		return renderer;
+	}
+
+	AbsoluteDimensions getScreenDimensions() {
+		int w, h;
+		if (SDL_GetRendererOutputSize(renderer, &w, &h)) {
+			std::string err = "Renderer size not available! SDL_Error: ";
+			throw std::runtime_error(err.append(SDL_GetError()));
+		}
+
+		return { static_cast<uint>(w), static_cast<uint>(h) };
+	};
+
+	GPUImage& getImage(Image& image) {
+		if (imageMap.contains(image.path)) {
+			return imageMap.at(image.path);
+		}
+		imageMap.insert({image.path, {image}});
+		return imageMap.at(image.path);
+	};
+
+	/**
+	* @brief Renders an Image at a specified position.
+	*
+	* @param src Source image.
+	* @param posMap Relative position mapping between source and destination.
+	* @param scale_percentage Percentage points to scale the source by before blitting.
+	* @return The return status code of the underlying SDL_BlitScaled function.
+	*/
+	int renderImage(Image src, PositionMapping posMap, uint scale_percentage) {
+
+		// The SDL gods demand a position sacrifice
+		SDL_Rect pos;
+
+
+		GPUImage image = getImage(src);
+
+		int w, h;
+		SDL_QueryTexture(image.getTexture(), nullptr, nullptr, &w, &h);
+
+		// Convenience variables
+		uint srcTextureWidth	= w;
+		uint srcTextureHeight	= h;
+
+		uint rendererWidth		= getScreenDimensions().w;
+		uint rendererHeight		= getScreenDimensions().h;
+
+		// Create both ratios now, to enable easier comparison with the later src ratios
+		double destWidthDivHeightRatio = static_cast<double>(rendererWidth) / static_cast<double>(rendererHeight);
+		double destHeightDivWidthRatio = static_cast<double>(rendererHeight) / static_cast<double>(rendererWidth);
+
+		// It's easier to define both the width/height and height/width ratios than it is to compute only one of the two, and then
+		// take the reciprocal of it. Besides, in cases of surfaces with extreme ratios, even though we're using doubles, one might end up with
+		// one of these being inaccurate. Note that this is purely theoretical, and that I reckon this will be fine for all practical purposes.
+		// Still neater code-wise though, so I'm keeping this for the foreseeable future.
+		double srcWidthDivHeightRatio = static_cast<double>(srcTextureWidth) / static_cast<double>(srcTextureHeight);
+		double srcHeightDivWidthRatio = static_cast<double>(srcTextureHeight) / static_cast<double>(srcTextureWidth);
+
+		// Reduce the integer percentage to a double we can multiply into other expressions
+		double scale = static_cast<double>(scale_percentage) / static_cast<double>(100.0);
+
+		// This is a tricky bit of application-specific code
+		// Essentially, if the aspect ratio of the src and dest mismatch, different space-filling methods must be used
+		// If srcW/srcH < destW/destH, or in other words, the source is less wide than the destination, it will fill the dest tall-wise
+		// In the opposite case, the source is wider, and it will fill up the width as much as possible
+		// If they are of equal aspect ratio, the method used is irrelevant, and either will work
+		// Note that the 'or' here is technically redundant. Either both checks will succeed, or both will fail.
+		// In cases of (hypothetical) float distortion, one of these checks might (hypothetically) fail, but both shouldn't
+		// Therefore there is an or here, though it is highly unlikely it would come to use.
+		if (srcHeightDivWidthRatio > destHeightDivWidthRatio || srcWidthDivHeightRatio < destWidthDivHeightRatio ) {
+			pos.w = static_cast<uint>(std::round(scale * rendererHeight * srcWidthDivHeightRatio));
+			pos.h = static_cast<uint>(std::round(scale * rendererHeight));
+		}
+		else {
+			pos.w = static_cast<uint>(std::round(scale * rendererWidth));
+			pos.h = static_cast<uint>(std::round(scale * rendererWidth * srcHeightDivWidthRatio));
+		}
+
+
+		AbsoluteDimensions srcDim = {
+			.w = static_cast<uint>(pos.w),
+			.h = static_cast<uint>(pos.h)
+		};
+
+		AbsoluteDimensions destDim = {
+			.w = static_cast<uint>(rendererWidth),
+			.h = static_cast<uint>(rendererHeight)
+		};
+		// Get x and y coordinates of the screen to be blitted
+		AbsolutePosition xy = getPixelPosfromPosition(srcDim, destDim, posMap);
+
+		pos.x = xy.x;
+		pos.y = xy.y;
+
+
+		return SDL_RenderCopy(renderer, image.getTexture(), nullptr, &pos);
+	};
+};
+
+
+
+
+void renderText(TextRenderer& textRenderer, GPURenderManager& renderManager, Dialogue dialogue, DialogueFont font) {
+	textRenderer.renderStoryFrame(dialogue, font);
+	// hack
+	AbsoluteDimensions d = renderManager.getScreenDimensions();
+	textRenderer.displayText({.x = 0, .y = static_cast<int>(0.75 * d.h)});
+	
+};
+
+
+void renderFrame(GPURenderManager& SDLInfo, Frame& curFrame, TextRenderer& textRenderer) {
+	/* Render loop:
+	   	
+		Background - x
+			|- Transforms
+			|- Transitions
+		Characters
+			|- Position - x
+			|- Multiple in a frame
+			|- Expressions - x
+			|		|- Expression builder?
+			|- Animations?
+			|- Transforms
+			|- Transitions
+		Dialogue
+			|- Text box - x
+			|- Text - x
+			|- Autofitting - x?
+			|- Custom design?
+		Graphical interface
+			|- Keymaps - x?
+			|- Menu
+			|- Mouse clickies
+			
+	*/
+
+	// Initial setup
+	SDL_Surface* screenSurface = SDLInfo.getScreenSurface();
+	SDL_Window* window = SDLInfo.getWindow();
+	GPUImage screen = SDLInfo.getScreenImage();
+
+	
+
+	// Background
+	std::cout << "background" << std::endl;
+
+	SDL_FillRect(screenSurface, nullptr, SDL_MapRGB(screenSurface->format, 0x00, 0x00, 0x00));
+	
+	PositionMapping posMap = {
+		.srcPos = {0.5, 0.5},
+		.destPos = {0.5, 0.5},
+	};
+	
+	if (blitImageConstAspectRatio(SDLInfo.getImage(curFrame.bg), screen, posMap, 100)) {
+		std::string err = "SDL error! Error string is ";
+		throw std::runtime_error(err.append(SDL_GetError()));
+	}
+
+	// Characters
+	std::cout << "characters" << std::endl;
+	// TODO: custom expression handlers 
+	if (blitImageConstAspectRatio(SDLInfo.getImage(curFrame.storyCharacter.expressions.at(curFrame.expression)), screen, curFrame.position, 80)) {
+		std::string err = "SDL error! Error string is ";
+		throw std::runtime_error(err.append(SDL_GetError()));
+	}
+	
+	// Text
+	std::cout << "text" << std::endl;
+	renderText(textRenderer, SDLInfo, {curFrame.storyCharacter.name, curFrame.textDialogue, {255, 255, 255}}, {"BonaNova-Italic.ttf"});
+	
+	std::cout << "flip buffers" << std::endl;
+	SDL_UpdateWindowSurface(window);
+
+}
+
+
+};
