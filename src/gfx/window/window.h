d273ab16734c035620ec8f751b41e9b55481721a
Canada Jones <canadajones68@gmail.com>
2023-10-09 22:52:15 +0200

    Added initial SDL code.

---
+++ src/gfx/window/window.h
@@ -0,0 +1,82 @@
+#ifndef VNPGE_SDL_WINDOW_HEADER
+#define VNPGE_SDL_WINDOW_HEADER
+
+#include <SDL2/SDL_render.h>
+#include <cassert>
+#include <memory>
+#include <stdexcept>
+
+
+#define SDL_MAIN_HANDLED
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_video.h>
+
+namespace gfx {
+namespace window {
+
+
+class SDLScope {
+	private:
+	static inline bool wasInit = false;
+	
+	public:
+	SDLScope() {
+		assert(!wasInit);
+
+		if (SDL_Init(SDL_INIT_AUDIO | SDL_INIT_VIDEO)) {
+			throw std::runtime_error(SDL_GetError());
+		}
+
+		wasInit = true;
+	}
+	~SDLScope() {
+		SDL_Quit();
+	}
+
+	SDLScope(const SDLScope&) = delete;
+	SDLScope(const SDLScope&&) = delete;
+	SDLScope& operator=(const SDLScope&) = delete;
+	SDLScope& operator=(const SDLScope&&) = delete;
+};
+
+class Window {
+	private:
+	std::unique_ptr<SDL_Window, decltype(&SDL_DestroyWindow)> window;
+	
+	public:
+	Window() : window{SDL_CreateWindow("test", 10, 10, 640, 480, SDL_WINDOW_SHOWN), SDL_DestroyWindow}{
+		if (!window) {
+			throw std::runtime_error(SDL_GetError());
+		}		
+	}
+	
+	SDL_Window* getWindow() {
+		return window.get();
+	}
+
+};
+
+
+class Renderer {
+	private:
+    std::unique_ptr<SDL_Renderer, decltype(&SDL_DestroyRenderer)> renderer;
+
+    public:
+    Renderer(Window& window) : renderer{SDL_CreateRenderer(window.getWindow(), -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC), SDL_DestroyRenderer} {
+        if (!renderer) {
+            throw std::runtime_error(SDL_GetError());
+        }
+    }
+
+    SDL_Renderer* getRenderer() {
+        return renderer.get();
+    }
+
+};
+
+
+
+}
+}
+
+#endif
---
+++ tests/gfx.sdl.test.cpp
@@ -0,0 +1,37 @@
+#include "gfx/window/window.h"
+#include "SDL2/SDL.h"
+#include "SDL2/SDL_video.h"
+#include "SDL2/SDL_rect.h"
+#include "SDL2/SDL_surface.h"
+#include <SDL2/SDL_render.h>
+
+namespace window = gfx::window;
+int main(int argc, char** argv) {
+	window::SDLScope scope{};
+	window::Window window{};
+	window::Renderer renderer{window};
+
+	SDL_Renderer* r = renderer.getRenderer();
+
+	SDL_Surface* surf = SDL_CreateRGBSurface(0, 200, 200, 32, 0, 0, 0, 0);
+
+	SDL_Rect rect =  {.x = 20, .y = 40, .w = 200, .h = 30};
+
+	SDL_FillRect(surf, nullptr, 0xFFFFFFFF);
+	SDL_FillRect(surf, &rect, 0x0C0C0CFF);
+
+	SDL_Texture* tex = SDL_CreateTextureFromSurface(r, surf);
+	
+	SDL_SetRenderDrawColor(r, 0xFF, 0xFF, 0xFF, 0xFF);
+	SDL_RenderClear(r);
+
+	SDL_RenderCopy(r, tex, nullptr, &rect);
+
+	SDL_RenderPresent(r);	
+
+	SDL_Delay(7000);
+
+	return 0;
+	
+	
+}
